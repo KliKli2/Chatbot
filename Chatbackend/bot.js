01332ffeeb55f0baf469fb739f32845985a133aa
KliKli2 <maximilian.hohenbuehel@gmail.com>
2021-07-16 19:48:12 +0200

    Increased dataset and cleaned up the bot.js logic

--- Chatbackend/bot.js
+++ Chatbackend/bot.js
@@ -14,6 +14,11 @@ class bot {
 	 */
 	constructor () {
 		this.dict = require("./bot.json")
+		this.usedKeyWords = []
+		this.name = "Chatbot"
+		this.links = []
+		this.images = []
+
 		/** Die Websocketverbindung
 		*/
 		this.client = new WebSocketClient()
@@ -39,15 +44,6 @@ class bot {
 				console.log('Connection Error: ' + error.toString())
 			})
 
-			// this.client.con.sendUTF('{"type": "msg", "name": "' + 
-			// 	'Chatbot' + 
-			// 	'", "msg": "' + 
-			// 	'Guten Tag' + 
-			// 	'", "images":"' + 
-			// 	[] + 
-			// 	'", "links":"' + 
-			// 	[] +
-			// 	'"}')
 			/** 
 			 * Es kann immer sein, dass sich der Client disconnected 
 			 * (typischer Weise, wenn der Server nicht mehr da ist)
@@ -63,8 +59,6 @@ class bot {
 			connection.on('message', function (message) {
 				if (message.type === 'utf8') {
 					var data = JSON.parse(message.utf8Data)
-					// console.log("Bot Data -> " + JSON.stringify(message))
-					// console.log('Received: ' + data.msg + ' ' + data.name + ' ' + data.video)
 				}
 			})
 
@@ -99,81 +93,135 @@ class bot {
 		this.connected = true
 	}
 
+	/*
+	 *	Function to return all countries that are attractive through certain characteristics
+	 * */
+	getCountryWithCharacteristic(character){
+		var ret = []
+		for(var i in countries){
+			if(this.dict.countries[i].character.includes(character)){
+				ret.push(this.dict.countries[i].name)
+			}
+		}
+		return ret
+	}
+
+	/*
+	 *	Function to return all countries that are visitable in a certain season
+	 * */
+	getCountryInSeason(season){
+		var ret = []
+		for(var i in countries){
+			if(this.dict.countries[i].season.includes(season)){
+				ret.push(this.dict.countries[i].name)
+			}
+		}
+		return ret
+	}
+
+	/*
+	 * Function to return a random element from a specific array
+	 */
+	getRandomElement(responses){
+		return responses[Math.floor(Math.random()*responses.length)]
+	}
+
+	/*
+	 *	Function to separate links, swaps certain keywords and formats the final message
+	 * */
+	postProcessMsg(msg){
+		if(!msg){
+		return '{"type": "msg", "name": "' + 
+			this.name + 
+			'", "msg": "' + 
+			this.getRandomElement(this.dict.noResponse) + 
+			'", "images":"' + 
+			this.images + 
+			'", "links":"' + 
+			this.links +
+			'"}'
+		}
+		msg = msg.replace("RSEASON", this.getRandomElement(this.dict.seasons))
+		msg = msg.replace("RCHARACTER", this.getRandomElement(this.dict.characters))
+		msg = msg.replace("RCOUNTRY", this.getRandomElement(this.getCountryNames()))
+		if(msg.search(/<[\s\S]+>/i) != -1){
+			this.links.push(msg.slice(msg.search("<") + 1, msg.search(">")))
+			msg = msg.replace(msg.slice(msg.search("<"), msg.search(">") + 1), "")
+		}
+		return '{"type": "msg", "name": "' + 
+			this.name + 
+			'", "msg": "' + 
+			msg + 
+			'", "images":"' + 
+			this.images + 
+			'", "links":"' + 
+			this.links +
+			'"}'
+	}
+
+	/*
+	 *	Function to search and return the list of available countries
+	 * */
+	getCountryNames(){
+		var ret = []
+		for(var i in this.dict.countries){
+			ret.push(this.dict.countries[i].name)
+		}
+		return ret
+	}
+
+	/*
+	 *	Function to determine if the sentence has one of the words included, that should trigger the word spotting algorithm
+	 *	It also checks for multiple usage of that specific word and returns the corresponding message
+	 * */
+	hasKeyword(sentence){
+
+		// countries = getCountryNames()
+		// console.log(this.getCountryInSeason("herbst"))
+		// for(var i in countries){
+		// 	if(nachricht.includes(countries[i])){
+		// 		for(var j in this.dict.countries){
+		// 			if(countries[j] == countries[i]){
+		// 				si = this.getRandomElement(Object.entries(this.dict.sights[j]))
+		// 				// console.log(si)
+		// 			}
+		// 		}
+		// 		msg = si[1][0]['definition'][0]
+		// 		// console.log(si[1][0]['definition'][0])
+		// 	}
+		// }
+
+
+		for (var i in this.dict.answers){
+			// console.log(dict.answers[i].keyword)
+			for(var j in this.dict.answers[i].keyword){
+				if(sentence.includes(this.dict.answers[i].keyword[j])){
+					if(!this.usedKeyWords.includes(this.dict.answers[i].keyword[j])){
+						this.usedKeyWords.push(this.dict.answers[i].keyword[j])
+						return this.getRandomElement(this.dict.answers[i].answer)
+					}else{
+						return this.getRandomElement(this.dict.answers[i].multiple)
+					}
+				}
+			}
+		}
+	}
+
 	/** 
 	 * Hier muss ihre Verarbeitungslogik integriert werden.
 	 * Diese Funktion wird automatisch im Server aufgerufen, wenn etwas ankommt, das wir 
 	 * nicht geschrieben haben
 	 * @param nachricht auf die der bot reagieren soll
 	 */
-	post (nachricht, greeting) {
-		if (greeting){
-			this.client.con.sendUTF('{"type": "msg", "name": "' + 
-				'Chatbot' + 
-				'", "msg": "' + 
-				'Guten Tag' + 
-				'", "images":"' + 
-				[] + 
-				'", "links":"' + 
-				[] +
-				'"}')
-		}else{
-			var name = 'Chatbot'
-			var msg = ''
-			var images = []
-			var links = []
+	post (nachricht) {
+		let msg = ''
+		this.links.length = 0
+		this.images.length = 0
 
-			/*
-			 * Verarbeitung
-			 */
-
-
-
-			// console.log(this.dict)
-			var countries = []
-			for(var i in this.dict.countries){
-				countries.push(Object.keys(this.dict.countries[i])[0])
-			}
-			// // console.log(countries)
-			var si
-
-			// console.log(si[1][0]['definition'][0])
-			// console.log(si[1][1]['preis'][0])
-			// console.log(si[1][2]['öffnung'][0])
-
-			function getSightData(sight){
-				// console.log(sight.length)
-				return sight[Math.floor(Math.random()*sight.length)]
-			}
-
-
-			// console.log(json.sights[0])
-
-			for(var i in countries){
-				if(nachricht.includes(countries[i])){
-					for(var j in this.dict.countries){
-						if(countries[j] == countries[i]){
-							si = getSightData(Object.entries(this.dict.sights[j]))
-							// console.log(si)
-						}
-					}
-					msg = si[1][0]['definition'][0]
-					// console.log(si[1][0]['definition'][0])
-				}
-			}
-
-
-			var msg = '{"type": "msg", "name": "' + 
-				name + 
-				'", "msg": "' + 
-				msg + 
-				'", "images":"' + 
-				images + 
-				'", "links":"' + 
-				links +
-				'"}'
-			// console.log('Send: ' + msg)
-			this.client.con.sendUTF(msg)
-		}
+		msg = this.hasKeyword(nachricht)
+		msg = this.postProcessMsg(msg)
+		// console.log('Send: ' + msg)
+		this.client.con.sendUTF(msg)
 	}
 
 }
